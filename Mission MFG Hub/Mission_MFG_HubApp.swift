e4c089e9c2ea899fff2bf005270f44e4dcde4f95
someotherdonkus <jake_sax@icloud.com>
2021-02-08 18:54:48 -0800

    Initial Commit

---
+++ Mission MFG Hub/Mission_MFG_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mission_MFG_HubApp.swift
+//  Mission MFG Hub
+//
+//  Created by Jacob Sax on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Mission_MFG_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
